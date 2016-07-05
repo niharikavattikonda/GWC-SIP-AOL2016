409cf3f979a79b224633fe9b491668ae67e19bdb
Niharika Vattikonda <niharika.vattikonda@gmail.com>
2016-07-05 17:59:30 -0400

    Update madLibsAriel.py

--- madLibsAriel.py
+++ madLibsAriel.py
@@ -1,52 +1,65 @@
 print("Get ready for a Mad Libs parody of Ariel's Part of Your World!\n")
 
-noun1 = input("Enter a noun ")
-adj1 = input("Enter an adjective ")
-adj2 = input("Enter an adjective ")
-noun2 = input("Enter a noun ")
-noun3 = input("Enter a noun ")
-noun4 = input("Enter a noun ")
-number1 = input("Enter a number as a word " )
-gerund1 = input("Enter a gerund ")
-noun5 = input("Enter a noun ")
-noun6 = input("Enter a noun ")
-noun7 = input("Enter a noun ")
-noun8 = input("Enter a noun ")
-noun9 = input("Enter a noun ")
-number2 = input("Enter a number as a word ")
-noun10 = input("Enter a noun ")
-gerund2 = input("Enter a gerund ")
-noun11 = input("Enter a noun ")
-verb1 = input("Enter a verb ")
-verb2 = input("Enter a verb ")
-verb3 = input("Enter a verb ")
-noun12 = input("Enter a noun ")
-gerund3 = input("Enter a gerund ")
+tryAgain = 1
 
-print("Look at this " + noun1 + ", isn't it " + adj1 + "?")
-print("Wouldn't you think my collection's " + adj2 + "?")
-print(""" Wouldn't you think I'm, the girl
-    The girl who has everything""")
+while tryAgain > 0:
+	noun1 = input("Enter a noun ")
+	adj1 = input("Enter an adjective ")
+	adj2 = input("Enter an adjective ")
+	noun2 = input("Enter a noun ")
+	noun3 = input("Enter a noun ")
+	noun4 = input("Enter a noun ")
+	number1 = input("Enter a number as a word " )
+	gerund1 = input("Enter a gerund ")
+	noun5 = input("Enter a noun ")
+	noun6 = input("Enter a noun ")
+	noun7 = input("Enter a noun ")
+	noun8 = input("Enter a noun ")
+	noun9 = input("Enter a noun ")
+	number2 = input("Enter a number as a word ")
+	noun10 = input("Enter a noun ")
+	gerund2 = input("Enter a gerund ")
+	noun11 = input("Enter a noun ")
+	verb1 = input("Enter a verb ")
+	verb2 = input("Enter a verb ")
+	verb3 = input("Enter a verb ")
+	noun12 = input("Enter a noun ")
+	gerund3 = input("Enter a gerund ")
 
-print("Look at this " + noun2 + ", " + noun3 + " untold")
-print("How many " + noun4 + " can " + number1 + " cavern hold?")
-print(gerund1 + " around here you'd think")
-print("Sure, she's got everything")
+	print("Look at this " + noun1 + ", isn't it " + adj1 + "?")
+	print("Wouldn't you think my collection's " + adj2 + "?")
+	print(""" Wouldn't you think I'm, the girl
+	The girl who has everything""")
 
-print("I've got " + noun5 + " and " + noun6 + " a plenty")
-print("I've got " + noun7 + " and " + noun8 + " galore")
-print("You want " + noun9 + "? I've got " + number2 + "!")
-print("""But who cares? No big deal,
-    I want more...""")
+	print("Look at this " + noun2 + ", " + noun3 + " untold")
+	print("How many " + noun4 + " can " + number1 + " cavern hold?")
+	print(gerund1 + " around here you'd think")
+	print("Sure, she's got everything")
 
-print("I wanna be where the " + noun10 + " are")
-print("I wanna see, wanna see them " + gerund2)
-print("""Walking around on those,
-    What do you call 'em?""")
-print("Oh, " + noun11)
+	print("I've got " + noun5 + " and " + noun6 + " a plenty")
+	print("I've got " + noun7 + " and " + noun8 + " galore")
+	print("You want " + noun9 + "? I've got " + number2 + "!")
+	print("""But who cares? No big deal,
+	I want more...""")
 
-print("Up where they " + verb1 + " , up where they " + verb2)
-print("Up where they " + verb3 + " all day in the " + noun12)
-print(gerund3 + " free")
-print("""Wish I could be,
-    part of that world""")
+	print("I wanna be where the " + noun10 + " are")
+	print("I wanna see, wanna see them " + gerund2)
+	print("""Walking around on those,
+	What do you call 'em?""")
+	print("Oh, " + noun11)
+
+	print("Up where they " + verb1 + " , up where they " + verb2)
+	print("Up where they " + verb3 + " all day in the " + noun12)
+	print(gerund3 + " free")
+	print("""Wish I could be,
+	part of that world""")
+	
+	tryQ = input("Would you like to try again? Select Y for Yes or N for no ")
+	if tryQ == "N":
+		tryAgain = 0
+		
+		print("Game over!")
+		
+	if tryQ == "Y":
+		tryAgain = 10
+		print("Let's try that again!")
